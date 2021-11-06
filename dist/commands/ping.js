ebc6c2eec6d8e982d6ba2a7cfa56253cf132c146
Marie <marie@kaifa.ch>
2021-11-06 18:58:23 +0100

    Delete ping.js

--- dist/commands/ping.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = {
-    name: "ping",
-    description: "test command",
-    execute: function (message, args) {
-        message.channel.send("passed");
-    },
-};
